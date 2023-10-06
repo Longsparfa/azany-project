5af1eedea1c5cf0b0402973cea9986b820f1c70a
Parfa Longs <longsparfa@gmail.com>
2023-10-06 11:21:00 +0100

    commit deleted slider data

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
-import { sliderData } from "./slider-data";
+import { art, freeInterest, avc } from "../../assets/index";
 
 const Slider = () => {
+  const sliderData = [art, freeInterest, avc];
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const sliderLength = sliderData.length;
@@ -16,35 +17,26 @@ const Slider = () => {
   };
 
   return (
-    <div className="w-[100%] h-[90vh] relative overflow-hidden bg-[#242424] ">
+    <div className="w-[100%] h-[30vh] relative overflow-hidden  ">
       <AiOutlineArrowLeft
-        className="border-solid border-[2px] border-[#00a35c] rounded-[50%] bg-transparent text-white w-[2.5rem] h-[2.5rem] cursor-pointer absolute top-[50%] translate-y-[50%] z-[2] hover:bg-[#fff] left-[1.5rem] text-[#00a35c] "
+        className="border-solid border-[2px] border-[#c41805] rounded-[50%] bg-[#1c0b1e] text-white w-[2.5rem] h-[2.5rem] cursor-pointer absolute top-[50%] translate-y-[50%] z-[2] hover:bg-[#fff] left-[1.5rem] text-[#00a35c] "
         onClick={prevSlide}
       />
       <AiOutlineArrowRight
-        className="border-solid border-[2px] border-[#00a35c] rounded-[50%] bg-transparent text-white w-[2.5rem] h-[2.5rem] cursor-pointer absolute top-[50%] translate-y-[50%] z-[2] hover:bg-[#fff] right-[1.5rem] text-[#00a35c] "
+        className="border-solid border-[2px] border-[#c41805] rounded-[50%] bg-[#1c0b1e] text-white w-[2.5rem] h-[2.5rem] cursor-pointer absolute top-[50%] translate-y-[50%] z-[2] hover:bg-[#fff] right-[1.5rem] text-[#00a35c] "
         onClick={nextSlide}
       />
-
-      {sliderData.map((slide, index) => {
-        const { image } = slide;
-        return (
-          <div
-            key={index}
-            className={
-              index === currentSlide
-                ? "absolute top-0 left-0 w-[100%] h-[100%] -translate-x-[50%] transition ease delay-0.5s opacity-1 translate-x-0"
-                : "absolute top-0 left-0 w-[100%] h-[100%] opacity-0 -translate-x-[50%] transition ease delay-0.5s"
-            }
-          >
-            {index === currentSlide && (
-              <>
-                <img src={image} alt="slide" className="w-[100%] h-[100%] " />
-              </>
-            )}
-          </div>
-        );
-      })}
+      return (
+      <>
+        <div className="absolute top-0 left-0 w-[100%] h-[100%] -translate-x-[50%] transition ease delay-0.5s opacity-1 translate-x-0">
+          <img
+            src={sliderData[currentSlide]}
+            alt="slide"
+            className="w-[100%] h-[100%] "
+          />
+        </div>
+      </>
+      );
     </div>
   );
 };
